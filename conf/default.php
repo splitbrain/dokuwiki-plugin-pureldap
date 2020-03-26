79f39653646dbe91196636a4fb016f788f558bf9
Andreas Gohr <andi@splitbrain.org>
2020-03-26 20:26:43 +0100

    wizard generated

---
+++ conf/default.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Default settings for the pureldap plugin
+ *
+ * @author Andreas Gohr <andi@splitbrain.org>
+ */
+
+//$conf['fixme']    = 'FIXME';
+
---
+++ lang/en/settings.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * english language file for pureldap plugin
+ *
+ * @author Andreas Gohr <andi@splitbrain.org>
+ */
+
+// keys need to match the config setting name
+// $lang['fixme'] = 'FIXME';
+
